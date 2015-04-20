6763dbecc38f2d1e66da8311ad56b141a1f1379c
AnCh <chugunov.anton1@gmail.com>
2015-04-20 19:11:25 +0300

    Working with private messaging

--- src/NodaChat/public/scripts/chat.js
+++ src/NodaChat/public/scripts/chat.js
@@ -1,16 +1,19 @@
 ﻿$(function () {
     
     var socket = io();
-
+    
     var $loginModal = $("#loginModal");
     var $privateMsgModal = $("#privateMsgModal");
-    var $privateMsgAction = $("#privateMsgAction");
+    var $privateMsgAction = $(".privateMsgAction");
+    var $btnSendPrivateMessagee = $("#btnSendPrivateMessagee");
     var $inputMessage = $("#inputMessage");
+    var $inputPrivateMessage = $("#inputPrivateMessage");
     var $btnSendMessage = $("#btnSendMessage");
     var $btnLogin = $("#btnLogin");
     var $inputNickname = $("#inputNickname");
     var $inputPassword = $("#inputPassword");
     var $chatMessages = $("#chatMessages");
+    var $privateChatMessages = $("#privateChatMessages");
     var $usersList = $("#usersList");
     
     var username;
@@ -22,13 +25,19 @@
         $chatMessages[0].scrollTop = $chatMessages[0].scrollHeight;
     }
     
+    function addPrivateChatMessageElement(el) {
+        var $el = $(el);
+        $privateChatMessages.append($el);
+        $privateChatMessages[0].scrollTop = $privateChatMessages[0].scrollHeight;
+    }
+    
     function addUsersElement(el) {
         var $el = $(el);
         $usersList.append($el);
         $usersList[0].scrollTop = $usersList[0].scrollHeight;
     }
     
-    function addChatMessage(data) {
+    function addChatMessage(data, privateMsg) {
         
         var avatarUrl = "../images/user.png";
         var $userAvatarImage = $("<img style='max-height:60px;' class=\"media-object img-circle\">").attr("src", avatarUrl);;
@@ -41,26 +50,49 @@
         
         var $messageDiv = $("<div class=\"media\">").append($userAvatar, $divMsg);
         var $mainLi = $("<li class=\"media\"/>").append($messageDiv);
-
+        
         addChatMessageElement($mainLi);
     }
+
+    function bindContextMenu() {
+        $("#context").contextmenu({
+            target: "#context-menu",
+            before: function (e, context) {
+                var id = context.parent().attr("id");
+                this.getMenu().find(".privateMsgAction").attr("id", id).html("Send private message to " + id);
+                return true;
+            }
+        });
+    }
     
-    function addUsersMessage(data) {
+    function addUserToActiveUsers(data) {
         
         var avatarUrl = "../images/user.png";
-        var $userAvatarImage = $("<img style='max-height:40px;' class=\"media-object img-circle\">").attr("src", avatarUrl);;
+        var $userAvatarImage = $("<img style='max-height:40px;' class=\"media-object img-circle\">").attr("src", avatarUrl);
         var $userAvatar = $("<a href='#' class=\"pull-left\">").append($userAvatarImage);
-
+        
         var role = "User";
         var message = data.username + " | " + role;
         var $msg = $("<h5>").text(message);
         var $divMsg = $("<div class=\"media-body\">").append($msg);
         
         var $mediaDiv = $("<div class=\"media\">").append($userAvatar, $divMsg);
-        var $messageDiv = $("<div class=\"media-body\" id='context' data-toggle='context' data-target='#context-menu'>").append($mediaDiv);
-        var $mainLi = $("<li class=\"media\"/>").append($messageDiv);
+        var $messageDiv = $("<div class=\"media-body\" id='context'>").append($mediaDiv);
+        var $mainLi = $("<li class=\"media\"/>").attr("id", data.username).append($messageDiv);
         
         addUsersElement($mainLi);
+        bindContextMenu();
+    }
+    
+    function addConnectedActiveUsers(connectedUsers) {
+        $.each(connectedUsers, function (index, value) {
+            var data = { username: value };
+            addUserToActiveUsers(data);
+        });
+    }
+    
+    function removeUserFromActiveUsers(data) {
+        $("#" + data.username).remove();
     }
     
     function updateChat(message) {
@@ -83,7 +115,6 @@
     }
     
     function sendMessage() {
-
         var message = $inputMessage.val();
         message = cleanInput(message);
         
@@ -99,12 +130,35 @@
             username: username,
             message: message
         });
+        socket.emit("new message", message);
+    }
+    
+    function sendPrivateMessage() {
+        var message = $inputPrivateMessage.val();
+        message = cleanInput(message);
         
+        if (!message) {
+            return;
+        }
+        if (!connected) {
+            return;
+        }
+        
+        $inputPrivateMessage.val("");
+        addChatMessage({
+            username: username,
+            message: message
+        });
         socket.emit("new message", message);
     }
     
     $loginModal.modal({ backdrop: "static" });
     
+    $privateMsgAction.click(function (e) {
+        $('#privateMsgModal .modal-title').html("Private chat with " + this.id);
+        $privateMsgModal.modal({ backdrop: "static" });
+    });
+
     $btnLogin.click(function () {
         var nickname = $inputNickname.val();
         var password = $inputPassword.val();
@@ -132,22 +186,23 @@
         sendMessage();
     });
     
-    $privateMsgAction.click(function () {
-        $privateMsgModal.modal({ backdrop: "static" });
+    $btnSendPrivateMessagee.click(function () {
+        sendPrivateMessage();
     });
     
     socket.on("login", function (data) {
         connected = true;
-        var message = "Welcome to chat – ";
+        var message = "Welcome to chat!";
         updateChat(message, { prepend: true });
         addParticipantsMessage(data);
-        addUsersMessage(data);
+        addConnectedActiveUsers(data.connectedUsers);
+        addUserToActiveUsers(data);
     });
     
     socket.on("user joined", function (data) {
         updateChat(data.username + " joined");
         addParticipantsMessage(data);
-        addUsersMessage(data);
+        addUserToActiveUsers(data);
     });
     
     socket.on("new message", function (data) {
@@ -157,5 +212,6 @@
     socket.on("user left", function (data) {
         updateChat(data.username + " left");
         addParticipantsMessage(data);
+        removeUserFromActiveUsers(data);
     });
 });
--- src/NodaChat/websockets/sockets.js
+++ src/NodaChat/websockets/sockets.js
@@ -4,7 +4,6 @@
     var numberOfUsers = 0;
 
     io.on("connection", function(socket) {
-
         var addedUser = false;
 
         socket.on("new message", function(data) {
@@ -13,29 +12,32 @@
                 message: data
             });
         });
+        
+        socket.on("new private message", function (data) {
+            io.to(socketid).emit('message', 'for your eyes only');
+        });
 
         socket.on("add user", function(username) {
             socket.username = username;
-
-            connectedUsers[username] = username;
             ++numberOfUsers;
             addedUser = true;
 
             socket.emit("login", {
                 numUsers: numberOfUsers,
+                connectedUsers: connectedUsers,
                 username: username
             });
             socket.broadcast.emit("user joined", {
                 username: socket.username,
                 numUsers: numberOfUsers
             });
+            connectedUsers[username] = username;
         });
 
         socket.on("disconnect", function() {
             if (addedUser) {
                 delete connectedUsers[socket.username];
                 --numberOfUsers;
-
                 socket.broadcast.emit("user left", {
                     username: socket.username,
                     numUsers: numberOfUsers
